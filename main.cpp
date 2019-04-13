f98bf4d16532651ade1e27818e62618d0b9630e9
kaetahr <a00825945@itesm.mx>
2019-04-13 11:26:02 -0500

    in reservas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "Fecha.h"
@@ -8,11 +9,86 @@ using namespace std;
 #include "Disco.h"
 #include "Software.h"
 
+bool cargaMateriales(Material *materiales[20], unsigned char &cant)
+{
+	ifstream inFile;
+	cant = 0;
+	inFile.open("Material.txt");
+	if (!inFile.is_open())
+	{
+		return false;
+	}
+	int id;
+	string tit;
+	char type;
+	while (inFile >> id)
+	{
+		inFile >> tit;
+		inFile >> type;
+
+		if (type == 'L')
+		{
+			int pgs;
+			string aut;
+			inFile >> pgs;
+			inFile >> aut;
+
+			Libro *temp;
+			temp = new Libro(id, tit, pgs, aut);
+			materiales[cant++] = temp;
+		}
+		else if (type == 'D')
+		{
+			int dur;
+			inFile >> dur;
+			Disco *temp;
+			temp = new Disco(id, tit, dur);
+			materiales[cant++] = temp;
+		}
+		else if (type == 'S')
+		{
+			string os;
+			inFile >> os;
+			Software *temp;
+			temp = new Software(id, tit, os);
+			materiales[cant++] = temp;
+		}
+	}
+
+	inFile.close();
+	return true;
+}
+
+bool cargaReservas(Reserva reservas[50], unsigned char &cant)
+{
+	ifstream inFile;
+	cant = 0;
+	inFile.open("Reserva.txt");
+	if (!inFile.is_open())
+	{
+		return false;
+	}
+
+	int d, m, a;
+	int idMat;
+	int idCli;
+	while (inFile >> d >> m >> a >> idMat >> idCli)
+	{
+		Fecha f(d, m, a);
+		reservas[cant].setFechaReservacion(f);
+		reservas[cant].setIDCliente(idCli);
+		reservas[cant++].setIDMaterial(idMat);
+	}
+	
+	inFile.close();
+	return true;
+}
+
 int main()
 {
 	Material *materiales[20];
 	unsigned char cantidadMateriales;
-	Reserva *reservas[50];
+	Reserva reservas[50];
 	unsigned char cantidadReservas;
 
 	//Input
@@ -27,6 +103,8 @@ int main()
 		cout << "No se encontro el archvo de reservas" << endl;
 		return 0;
 	}
+
+	// menu();
 	
 	return 0;
 }
