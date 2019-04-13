48195145b60970946f7a4a38a539eba87a6bc018
kaetahr <a00825945@itesm.mx>
2019-04-13 11:14:13 -0500

    in materiales

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+#include "Fecha.h"
+#include "Reserva.h"
+#include "Material.h"
+#include "Libro.h"
+#include "Disco.h"
+#include "Software.h"
+
 int main()
 {
+	Material *materiales[20];
+	unsigned char cantidadMateriales;
+	Reserva *reservas[50];
+	unsigned char cantidadReservas;
+
+	//Input
+	if(!cargaMateriales(materiales, cantidadMateriales))
+	{
+		cout << "No se encontro el archivo de materiales" << endl;
+		return 0;
+	}
+
+	if(!cargaReservas(reservas, cantidadReservas))
+	{
+		cout << "No se encontro el archvo de reservas" << endl;
+		return 0;
+	}
+	
 	return 0;
 }
